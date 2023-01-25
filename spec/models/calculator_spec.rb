b0e527e034f28c78764bb8c02e620c4741055657
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-01-25 17:32:40 -0300

    add test cases for Calculator.parse

--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -4,12 +4,64 @@ require_relative '../spec_helper'
 
 describe Calculator do
   describe '#parse' do
-    let(:input_string) { '15 20 32 56 89 45' }
+    context 'when parsing' do
+      let(:input_string) { '15 20 32 56 89 45' }
 
-    it 'returns an array' do
-      operands = Calculator.parse(input_string)
+      it 'returns an array' do
+        operands = Calculator.parse(input_string)
 
-      expect(operands.class.name).to eq('Array')
+        expected_result = [15.0, 20.0, 32.0, 56.0, 89.0, 45.0]
+
+        expect(operands).to eq(expected_result)
+      end
+    end
+
+    context 'when there are negative numbers' do
+      let(:input_string) { '-15 20 -32 -56 89 -54' }
+
+      it 'returns an array' do
+        operands = Calculator.parse(input_string)
+
+        expected_result = [-15.0, 20.0, -32.0, -56.0, 89.0, -54.0]
+
+        expect(operands).to eq(expected_result)
+      end
+    end
+
+    context 'when there are numbers with decimal places' do
+      let(:input_string) { '15.2 -10.0 9.1 3.5 7.8 -0.2 0.0 -0.75' }
+
+      it 'returns an array' do
+        operands = Calculator.parse(input_string)
+
+        expected_result = [15.2, -10.0, 9.1, 3.5, 7.8, -0.2, 0.0, -0.75]
+
+        expect(operands).to eq(expected_result)
+      end
+    end
+
+    context 'when there are numbers with exponential notation' do
+      let(:input_string) { '1.25e3 1 -8.4 0.2e1 -8.45e5 1000e0 1e1' }
+
+      it 'returns an array' do
+        operands = Calculator.parse(input_string)
+
+        expected_result = [1_250.0, 1.0, -8.4, 2.0, -845_000.0, 1_000.0, 10.0]
+
+        expect(operands).to eq(expected_result)
+      end
+    end
+
+    context 'when there are numbers with exponential notation and negative exponent' do
+      let(:input_string) { '1.25e-3 1e-4 -8.4 0.2e1 -8.45e-5 1000e-0 1e1' }
+
+      it 'returns an array' do
+        operands = Calculator.parse(input_string)
+
+        expected_result = [0.00125, 0.0001, -8.4, 2.0, -8.45e-5, 1_000.0, 10.0]
+
+        expect(operands).to eq(expected_result)
+      end
     end
   end
 
